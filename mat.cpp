228e39921619c83cdc364a36e6efa346ccc5cb46
Subramanian Venkatesan <subbu@sub.local>
2013-03-04 01:04:18 -0500

    Added variable step size

--- mat.cpp
+++ mat.cpp
@@ -10,7 +10,7 @@
 #include "matrix.h"
 #include <cfloat>
 #include <math.h>
-#define STEP_SIZE 0.0001
+#define STEP 0.001
 #define EPS 1.0e-20
 Matrix gradient(Matrix A,Matrix x,Matrix b)
 {
@@ -21,8 +21,17 @@ Matrix gradient(Matrix A,Matrix x,Matrix b)
 double function_val(Matrix A,Matrix x , Matrix b)
 {
      Matrix c = 0.5 * x.transpose() * A * x - b * x;
-    cout << c[0][0] ;
+//    cout << c[0][0] ;
     return c[0][0];
+}
+double step(Matrix A,Matrix x,Matrix b)
+{
+   Matrix r = b - A*x;
+   Matrix nu = r.transpose() * r ;
+   Matrix de = r.transpose() * A * r;
+  
+   return nu[0][0]/de[0][0];  
+   
 }
 /*bool is_same(Matrix x , Matrix  y)
 {
@@ -67,17 +76,28 @@ int main()
       x[i][0] = 0;
         
     }
+   if(A.determinant()==0)
+   {
+     cout<<"No Solution"<<endl;
+     abort();
+  }
     double old_norm = DBL_MAX;
     Matrix s=gradient(A,x,b);
     double new_norm =s.norm();  
-   cout<<"Calculating";
+  //  cout<<step(A,x,b);
+    cout<<"Calculating";
+    double step_size = step (A,x,b);    
   while( (old_norm - new_norm)  > EPS)
   {
        
      old_norm = new_norm; 
-     x = x - STEP_SIZE*s;
+     x = x - STEP * s;
      s = gradient(A,x,b);
+     step_size = step(A,x,b);
+     
+    // cout<<s;
      new_norm = s.norm(); 
+   //  break;
   }
   cout<<endl<<"Solution";
   cout << x ; 
